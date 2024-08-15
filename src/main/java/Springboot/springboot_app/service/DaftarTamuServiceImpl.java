e5db058c14c59af350c7cee20f9d28ee62e67d69
yiigii <wahyuaigi2@gmail.com>
2024-08-15 20:54:11 +0700

    upgrade project

--- src/main/java/Springboot/springboot_app/service/DaftarTamuServiceImpl.java
+++ src/main/java/Springboot/springboot_app/service/DaftarTamuServiceImpl.java
@@ -1,8 +1,11 @@
 package Springboot.springboot_app.service;
+
 import Springboot.springboot_app.model.DaftarTamu;
 import Springboot.springboot_app.model.Perusahaan;
 import Springboot.springboot_app.model.User;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,38 +14,44 @@ import java.util.Optional;
 @Service
 public class DaftarTamuServiceImpl implements DaftarTamuService {
 
-        private List<User> userList;
-        private List<Perusahaan> perusahaanList;
-        private List<DaftarTamu> daftarTamuList;
+    private List<User> userList;
+    private List<Perusahaan> perusahaanList;
+    private List<DaftarTamu> daftarTamuList;
 
-        public DaftarTamuServiceImpl() {
-            userList = new ArrayList<>();
-            perusahaanList = new ArrayList<>();
-            daftarTamuList = new ArrayList<>();
+    public DaftarTamuServiceImpl() {
+        userList = new ArrayList<>();
+        perusahaanList = new ArrayList<>();
+        daftarTamuList = new ArrayList<>();
 
-            User user1 = new User(1, "Aigi");
-            User user2 = new User(2, "Wahyu");
-            User user3 = new User(3, "Way");
-            User user4 = new User(4, "Muhammad");
+        User user1 = new User(1, "Aigi");
+        User user2 = new User(2, "Wahyu");
+        User user3 = new User(3, "Way");
+        User user4 = new User(4, "Muhammad");
 
-            Perusahaan perusahaan1 = new Perusahaan(1, "Skolla");
-            Perusahaan perusahaan2 = new Perusahaan(2, "Ruang Guru");
+        Perusahaan perusahaan1 = new Perusahaan(1, "Skolla");
+        Perusahaan perusahaan2 = new Perusahaan(2, "Ruang Guru");
 
-            Springboot.springboot_app.model.DaftarTamu daftarTamu1 = new Springboot.springboot_app.model.DaftarTamu(1, perusahaan1, user1);
-            Springboot.springboot_app.model.DaftarTamu daftarTamu2 = new Springboot.springboot_app.model.DaftarTamu(2, perusahaan1, user2);
-            Springboot.springboot_app.model.DaftarTamu daftarTamu3 = new Springboot.springboot_app.model.DaftarTamu(3, perusahaan2, user3);
-            Springboot.springboot_app.model.DaftarTamu daftarTamu4 = new Springboot.springboot_app.model.DaftarTamu(4, perusahaan2, user4);
+        DaftarTamu daftarTamu1 = new DaftarTamu(1, perusahaan1, user1);
+        DaftarTamu daftarTamu2 = new DaftarTamu(2, perusahaan1, user2);
+        DaftarTamu daftarTamu3 = new DaftarTamu(3, perusahaan2, user3);
+        DaftarTamu daftarTamu4 = new DaftarTamu(4, perusahaan2, user4);
 
-            daftarTamuList.add(daftarTamu1);
-            daftarTamuList.add(daftarTamu2);
-            daftarTamuList.add(daftarTamu3);
-            daftarTamuList.add(daftarTamu4);
+        daftarTamuList.add(daftarTamu1);
+        daftarTamuList.add(daftarTamu2);
+        daftarTamuList.add(daftarTamu3);
+        daftarTamuList.add(daftarTamu4);
 
-                for (DaftarTamu daftarTamu : daftarTamuList) {
-                System.out.println(daftarTamu);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        for (DaftarTamu daftarTamu : daftarTamuList) {
+            try {
+                String json = objectMapper.writeValueAsString(daftarTamu);
+                System.out.println(json);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
             }
-
         }
+    }
 
     @Override
     public Optional<User> getUser(Integer id) {
@@ -63,5 +72,4 @@ public class DaftarTamuServiceImpl implements DaftarTamuService {
     public void setDaftarTamu(DaftarTamu daftarTamu) {
         daftarTamuList.add(daftarTamu);
     }
-
 }
